30fb99086b35c5fa492830cb6dfcc28548ae1f88
= <=>
2025-02-25 18:41:54 -0500

    update the frontend so it runs, sorry for screwing up the vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: '.', // Ensure it looks at the correct directory
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'public/index.html', // Explicitly set the input path
-    }
-  }
-});
+  plugins: [react()],
+})
