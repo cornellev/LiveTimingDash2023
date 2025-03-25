da5bca200cda965413ff58709ba91371d868fb93
Jerry Ji <104660649+jji-bigG@users.noreply.github.com>
2025-03-26 06:30:47 +0800

    updated data_format

--- backend/src/mobile_dash.py
+++ backend/src/mobile_dash.py
@@ -14,19 +14,20 @@ async def insert_uc24(request: Request):
         raise HTTPException(status_code=400, detail=f"Invalid JSON payload: {e}")
  
     try: 
-         steer_angle = data['steer_angle']
          left_rpm = data['left_rpm']
          right_rpm = data['right_rpm']
-         brake_on = data['brake_on']
-         accel = data['accel']
+         gps_lat = data['gps_lat']
+         gps_long = data['gps_long']
+         x_accel = data['x_accel']
+         y_accel = data['y_accel']
+         z_accel = data['z_accel']
          temp = data['temp']
-         voltage = data['voltage']
     except KeyError as e:
          missing_field = e.args[0]
          raise HTTPException(status_code=422, detail=f"Missing required field: {missing_field}")
  
-    print(f"Steering Angle: {steer_angle}; Left RPM: {left_rpm}; Right RPM: {right_rpm}; "
-           f"Brake On: {brake_on}; Accelerometer: {accel}; Temperature: {temp}; Voltage: {voltage}")
+    #print(f"Steering Angle: {steer_angle}; Left RPM: {left_rpm}; Right RPM: {right_rpm}; "
+           #f"Brake On: {brake_on}; Accelerometer: {accel}; Temperature: {temp}; Voltage: {voltage}")
 
     # Send received data to all connected WebSocket clients
     for connection in active_connections:
@@ -35,13 +36,14 @@ async def insert_uc24(request: Request):
     
     return {
          "message": "Sensor values received", 
-         "steer_angle": steer_angle, 
+         "gps_lat": gps_lat, 
+        "gps_long":gps_long,
          "left_rpm": left_rpm, 
          "right_rpm": right_rpm, 
-         "brake_on": brake_on, 
-         "accel": accel, 
+         "x_accel": x_accel, 
+         "y_accel": y_accel, 
+         "z_accel": z_accel, 
          "temp": temp, 
-         "voltage": voltage
      }
 
 
